335f982bf5ed4ee7990f4ffade71ff9df10d0ff7
Yermakovm <yermakov.m.o@gmail.com>
2020-06-06 15:10:27 +0300

    TodoApp clean

---
+++ src/TofoApp.js
@@ -0,0 +1,31 @@
+import React, {useState} from "react"
+import Paper from "@material-ui/core/Paper"
+import Typography from "@material-ui/core/Typography"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import Grid from "@material-ui/core"
+function TodoApp(){
+    const initialTodos = [
+        {id: 1, task: "Wake up", complited: false},
+        {id: 2, task: "Have breakfast", complited: false},
+        {id: 3, task: "Take a shower", complited: false}
+    ]
+    const [todos, setTodos] = useState(initialTodos);
+    return(
+        <Paper
+            style ={{
+                padding:0,
+                margin:0,
+                height: "100px",
+                backgroundColor:"#fafafa"}}
+            elevation = {0}>
+            <AppBar color='primary' position = 'static' style ={{height:"64px"}}>
+                <Toolbar>
+                    <Typography color='inherit'> TODOS WITH HOOKS</Typography>
+                </Toolbar>
+            </AppBar>
+            <TodoList todos={todos}/>
+        </Paper>
+    )
+}
+export default TodoApp;
